499c0ee62e0bc7fdb13933d71fbff1d0815137e8
Alexandr Neveroff <neveroff1976@gmail.com>
2023-05-31 17:04:26 +0300

    Registration styled

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { ImageBackground, StyleSheet, View } from "react-native";
 import RegistrationScreen from "./Screens/RegistrationScreen";
-import BackgroundImage from "./images/background.jpg";
+import BackgroundImage from "./assets/images/background.jpg";
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Inter-Medium": require("./assets/fonts/Inter-Medium.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "SFProDisplay-Light": require("./assets/fonts/SFProDisplay-Light.ttf"),
+    "SFProDisplay-Regular": require("./assets/fonts/SFProDisplay-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
+    <View>
       <ImageBackground
         source={BackgroundImage}
         resizeMode="cover"
@@ -17,10 +31,5 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
   background: { width: "100%", height: "100%" },
 });
--- Screens/RegistrationScreen.jsx
+++ Screens/RegistrationScreen.jsx
@@ -2,17 +2,20 @@ import React, { useState } from "react";
 import {
   View,
   TextInput,
-  Button,
   StyleSheet,
   Text,
   TouchableOpacity,
+  Image,
 } from "react-native";
+import AvatarEmpty from "../assets/images/avatar-blanc.jpg";
+import AddIcon from "../assets/images/add.png";
 
 const RegistrationScreen = () => {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [avatar, setAvatar] = useState(null);
 
   const handleUsernameChange = (text) => {
     setUsername(text);
@@ -30,6 +33,10 @@ const RegistrationScreen = () => {
     setShowPassword(!showPassword);
   };
 
+  const handleAddPhoto = () => {
+    // Тут потім буде логіка додавання фото
+  };
+
   const handleRegister = () => {
     // Тут потім буде логіка переходу на потрібний екран
     console.log("Регистрация:", username, password, email);
@@ -37,49 +44,157 @@ const RegistrationScreen = () => {
 
   return (
     <View style={styles.container}>
-      <Text>Реєстрація</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Логін"
-        onChangeText={handleUsernameChange}
-        value={username}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Адреса електронної пошти"
-        onChangeText={handleEmailChange}
-        value={email}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Пароль"
-        secureTextEntry={!showPassword}
-        onChangeText={handlePasswordChange}
-        value={password}
-      />
-      <TouchableOpacity onPress={handleTogglePasswordVisibility}>
-        <Text style={styles.showPasswordText}>
-          {showPassword ? "Приховати" : "Показати"}
-        </Text>
+      <View style={styles.avatarContainer}>
+        {avatar ? (
+          <Image
+            source={{ uri: avatar }}
+            resizeMode="cover"
+            style={styles.avatarImage}
+          />
+        ) : (
+          <Image
+            source={AvatarEmpty}
+            resizeMode="cover"
+            style={styles.avatarImage}
+          />
+        )}
+        <TouchableOpacity style={styles.addButton} onPress={handleAddPhoto}>
+          <Image source={AddIcon} />
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.title}>Реєстрація</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Логін"
+          onChangeText={handleUsernameChange}
+          value={username}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Адреса електронної пошти"
+          onChangeText={handleEmailChange}
+          value={email}
+        />
+        <View style={styles.passwordContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Пароль"
+            secureTextEntry={!showPassword}
+            onChangeText={handlePasswordChange}
+            value={password}
+          />
+          <TouchableOpacity
+            onPress={handleTogglePasswordVisibility}
+            style={styles.showPasswordButton}
+          >
+            <Text style={styles.showPasswordText}>
+              {showPassword ? "Приховати" : "Показати"}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <TouchableOpacity
+        style={styles.registrationButton}
+        onPress={handleRegister}
+      >
+        <Text style={styles.registrationButtonText}>Зареєстуватися</Text>
       </TouchableOpacity>
-      <Button title="Зареєстуватися" onPress={handleRegister} />
-      <Text>Вже є акаунт? Увійти</Text>
+      <Text style={styles.registrationLink}>Вже є акаунт? Увійти</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: "center",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 549,
+    backgroundColor: "#fff",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
     paddingHorizontal: 16,
   },
+  avatarContainer: {
+    position: "absolute",
+    top: -60,
+    alignSelf: "center",
+    width: 132,
+    height: 120,
+  },
+  avatarImage: { width: 120, height: 120, borderRadius: 16 },
+  addButton: {
+    width: 25,
+    height: 25,
+    position: "absolute",
+    right: 0,
+    bottom: 14,
+    zIndex: 10,
+  },
+  title: {
+    fontFamily: "Roboto-Medium",
+    fontSize: 30,
+    lineHeight: 35,
+    textAlign: "center",
+    letterSpacing: 0.01,
+    color: "#212121",
+    marginBottom: 33,
+    marginTop: 92,
+  },
+  inputContainer: {
+    gap: 16,
+    marginBottom: 43,
+  },
   input: {
-    height: 40,
-    borderColor: "gray",
+    height: 50,
+    borderColor: "#E8E8E8",
+    backgroundColor: "#F6F6F6",
     borderWidth: 1,
-    marginBottom: 12,
-    paddingHorizontal: 8,
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#BDBDBD",
+  },
+
+  passwordContainer: {
+    position: "relative",
+  },
+  showPasswordButton: {
+    position: "absolute",
+    top: 10,
+    right: 10,
+    padding: 4,
+  },
+  showPasswordText: {
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#1B4371",
+  },
+  registrationButton: {
+    paddingHorizontal: 32,
+    paddingVertical: 16,
+    backgroundColor: "#FF6C00",
+    borderRadius: 100,
+    marginBottom: 16,
+  },
+  registrationButtonText: {
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    lineHeight: 19,
+    textAlign: "center",
+    color: "#FFFFFF",
+  },
+  registrationLink: {
+    fontFamily: "Roboto-Regular",
+    fontSize: 16,
+    lineHeight: 19,
+    color: "#1B4371",
+    textAlign: "center",
   },
 });
 
