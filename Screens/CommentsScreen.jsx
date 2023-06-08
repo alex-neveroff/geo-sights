cdc251f6e7cb7295e2f078ddbaf6f1fc9083a739
Alexandr Neveroff <neveroff1976@gmail.com>
2023-06-08 20:37:02 +0300

    create comment screen

--- Screens/CommentsScreen.jsx
+++ Screens/CommentsScreen.jsx
@@ -1,10 +1,38 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { useRoute } from "@react-navigation/core";
 
 const CommentsScreen = () => {
+  const {
+    params: { comments, photo },
+  } = useRoute();
+
+  const [newComment, setNewComment] = useState({
+    text: "",
+    date: "",
+    avatar: {
+      uri: "",
+    },
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Comments Screen</Text>
+      <Image source={photo} resizeMode="cover" style={styles.postImage} />
+      <View style={styles.commentsContainer}>
+        {comments.map((comment, index) => (
+          <View style={styles.comment} key={index}>
+            <Image
+              source={comment.avatar}
+              resizeMode="cover"
+              style={styles.commentAvatar}
+            />
+            <View>
+              <Text style={styles.commentNext}>{comment.text}</Text>
+              <Text style={styles.commentDate}>{comment.date}</Text>
+            </View>
+          </View>
+        ))}
+      </View>
     </View>
   );
 };
@@ -12,9 +40,11 @@ const CommentsScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 32,
+    paddingHorizontal: 16,
+    backgroundColor: "#FFFFFF",
   },
+  postImage: { width: "100%", height: 240, borderRadius: 8, marginBottom: 8 },
 });
 
 export default CommentsScreen;
